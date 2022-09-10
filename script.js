e154d4ef0fe166bdfb7605638f1de7cfb6bdad00
RahulDhiman348 <102219032+RahulDhiman348@users.noreply.github.com>
2022-09-10 23:48:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let inputColor = document.getElementById('color-input');
+let inputSize = document.getElementById('brush-size');
+let inputInnerColor = document.getElementById('inner-color');
 ctx.globalCompositeOperation = 'luminosity';
-//https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
-
-let colorInput = prompt("Pleasr Enter the color you need");
-
-const edge = 70;
+const drawbtn = document.getElementById('start');
+const resetbtn = document.getElementById('rst');
+let color= 'red';
+let innerColor= 'white';
+let edge = 50;
 let drawing = false;
+//https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
+//inner color is working
+function startDrawing(){
+   color  = inputColor.value;
+   innerColor = inputInnerColor.value;
+   edge       = inputSize.value;
+    
+}
+
+function resetCanvas(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+}
 
 //setting up object for mouse position
 const mouse = {
@@ -26,10 +41,11 @@ window.addEventListener('mousemove', function(event){
 // randomized particles
 
 class Root {
-     constructor(x, y, color, centerX, centerY){
+     constructor(x, y, color, inrColor, centerX, centerY){
         this.x = x;
         this.y = y;
         this.color = color;
+        this.inrColor= inrColor;
         this.centerX = centerX;
         this.centerY = centerY;
         this.speedX = 0;
@@ -37,16 +53,18 @@ class Root {
      }
 
      draw(){
-        this.speedX += (Math.random()-0.5) /2;
-        this.speedY += (Math.random()-0.5) /2;
+        this.speedX += (Math.random()-0.5) ;
+        this.speedY += (Math.random()-0.5) ;
         this.x += this.speedX;
         this.y += this.speedY;
+        this.color= color;
+        this.innerColor= innerColor;
         
         const distanceX = this.x - this.centerX;
         //distance from center of particle to x position
         const distanceY = this.y - this.centerY;
         //distance from center of particle to y position
-        const distance = Math.sqrt(distanceX*distanceX + distanceY*distanceY);
+        const distance = (Math.sqrt(distanceX*distanceX + distanceY*distanceY));
         //using pythagoras theorem to calculate distance
         // basically hypotenuse formed by traingle of xand y position
         const radius = (-distance/edge +1)*edge / 10;
@@ -57,7 +75,7 @@ class Root {
             ctx.arc(this.x, this.y, radius, 0, 2*Math.PI);
             ctx.fillStyle = this.color;
             ctx.fill();
-            ctx.strokeStyle= 'black';
+            ctx.strokeStyle= innerColor;
             ctx.stroke();
         }
      }
@@ -67,7 +85,7 @@ function branchOut(){
     const centerX = mouse.x;
     const centerY = mouse.y;
     for(let i=0;i<3;i++){
-        const root = new Root(mouse.x, mouse.y, colorInput, centerX, centerY);
+        const root = new Root(mouse.x, mouse.y, innerColor, color, centerX, centerY);
         root.draw();
     }
 }
@@ -79,8 +97,8 @@ window.addEventListener('resize', function(){
 });
 
 window.addEventListener('mousemove', function(){
-   // ctx.fillStyle= 'rgba(255,255,255,0.03)';
-   // ctx.fillRect(0, 0, canvas.width, canvas.height);
+ //  ctx.fillStyle= 'rgba(255,255,255,0.03)';
+  //  ctx.fillRect(0, 0, canvas.width, canvas.height);
     
     branchOut();
 });
